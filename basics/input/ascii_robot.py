1707c6bbf6757fc23724d66b5768cb6f7ca81a25
samcole8 <sam@samcole.me>
2022-10-18 23:28:07 +0100

    Modified comments

--- basics/input/ascii_robot.py
+++ basics/input/ascii_robot.py
@@ -1,7 +1,8 @@
-# Display a box
+# Ask user for eye character
 asciiEye = input("Please enter a character for the eye\n")
 print("Beep's expression is now as follows:")
 print(" ########")
+# Insert eye characters to string
 print(f" # {asciiEye}  {asciiEye} #")
 print(" #  __  #")
 print(" ########")
