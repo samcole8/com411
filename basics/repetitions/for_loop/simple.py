57194317a9be70469d4fae6ba6a6e63d2115c9e0
samcole8 <sam@samcole.me>
2022-10-19 19:46:48 +0100

    Added code to simple.py

---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,9 @@
+"""
+Demonstrates the use of a simple for loop
+"""
+
+mountain_number = int(input("How many mountains should I display?\n"))
+print("Displaying...\n")
+for i in range(0, mountain_number):
+    print("mountain")
+print("\nDone!")
