b2255240cdaea22b58882df89902dd94f7c5b705
samcole8 <sam@samcole.me>
2022-10-19 18:45:29 +0100

    Added code to and_operator.py

---
+++ basics/decisions/and_operator.py
@@ -0,0 +1,11 @@
+"""
+Demonstrates use of logical and operator
+"""
+
+heard = input("What did I hear?\n")
+seen = input("\nWhat did I see?\n")
+
+if (heard == "grrr") and (seen == "big red eyes"):
+    print("\nThere is a scary creature. I should get out of here!")
+else:
+    print("I am a little scared but I will continue.")
