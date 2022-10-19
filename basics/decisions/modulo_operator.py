c8fe177cc7bea682faafc2b079ef133a552f0172
samcole8 <sam@samcole.me>
2022-10-19 13:35:05 +0100

    Created modulo_operator.py and added code demonstrating use of a modulo operator to determine whether the number is odd or even.

---
+++ basics/decisions/modulo_operator.py
@@ -0,0 +1,9 @@
+"""
+Use modulo operator to determine whether a number is odd or even.
+"""
+
+number = int(input("Please enter a whole number:\n"))
+if (number % 2) == 0:
+    print(f"\nThe number {number} is an even number.")
+else:
+    print(f"\nThe number {number} is an odd number.")
