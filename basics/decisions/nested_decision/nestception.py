08817276447b2702bb9d380e674699f307add3c2
samcole8 <sam@samcole.me>
2022-10-19 14:34:19 +0100

    Added code to nestception.py

---
+++ basics/decisions/nested_decision/nestception.py
@@ -0,0 +1,20 @@
+"""
+Demonstrates multiple nested if statements
+"""
+
+user_input = input("Where should I look?\n")
+if user_input == "in the bedroom":
+    if input("Where in the bathroom should I look?\n") == "under the bed":
+        print("Found some shoes but no battery.")
+    else:
+        print("Found some mess but no battery.")
+elif user_input == "in the bathroom":
+    if input("Where in the bathroom should I look\n") == "in the bathtub":
+        print("Found a rubber duck but no battery.")
+    else:
+        print("Found a wet surface but no battery.")
+elif user_input == "in the lab":
+    if input("Where in the lab should I look?\n") == "on the table":
+        print("Yes! I found my battery!")
+    else:
+        print("Found some tools but no battery.")
