ce6ee4442b329ab88bb11b2f8f58d1d89c7f23f1
samcole8 <sam@samcole.me>
2022-10-19 21:26:52 +0100

    Added code to membership_operators.py

---
+++ basics/repetitions/for_loop/membership_operators.py
@@ -0,0 +1,10 @@
+"""
+Script to reverse a string using membership operators.
+"""
+
+forward_phrase = input("What phrase do you see?\n")
+print("\nReversing")
+backward_phrase = "" # pylint: disable-msg=C0103
+for character in forward_phrase:
+    backward_phrase = character + backward_phrase
+print(f"\nThe phrase is {backward_phrase}")
