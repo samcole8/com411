5aa4be15d77284d55028d1b7de83f20ac1885e33
samcole8 <sam@samcole.me>
2022-10-19 19:01:05 +0100

    Fixed logical error in count.py

--- basics/repetitions/while_loo/count.py
+++ basics/repetitions/while_loo/count.py
@@ -5,7 +5,7 @@ Demonstrates counting with a simple while loop
 number_of_cables = int(input("How many live cables should I avoid?\n"))
 print()
 count = 0 # pylint: disable-msg=C0103
-while count <= number_of_cables:
+while count < number_of_cables:
+    count += 1
     print(f"Avoiding...Done! {count} live cables avoided.")
-    count -= 1
 print("All live cables have been avoided.")
