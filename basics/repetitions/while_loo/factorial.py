512b23bb9e31bc69bedeac7d871739fe558cafec
samcole8 <sam@samcole.me>
2022-10-19 19:35:52 +0100

    Added code to factorial.py

---
+++ basics/repetitions/while_loo/factorial.py
@@ -0,0 +1,10 @@
+"""
+Script to calculate to factorial (x!) of number x
+"""
+
+x = int(input("Please enter a number:\n"))
+factorial = 1 # pylint: disable-msg=C0103
+while x > 0:
+    factorial *= x
+    x -= 1
+print(f"\nThe factorial is {factorial}.")
