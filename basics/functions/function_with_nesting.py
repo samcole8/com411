995d22009ecad1d1cf9c804b6a4c51ca23ee97f3
samcole8 <sam@samcole.me>
2022-10-19 23:44:51 +0100

    Added code to function_with_nesting.py

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,11 @@
+"""Simple script demonstrating a function with a nested decision statement"""
+
+def identify():
+    """Example if statement"""
+    seen = input("What lies before us? \n")
+    if seen == "a large boulder":
+        print("\nIt's time to run!")
+    else:
+        print("\nWe will be fine.")
+
+identify()
