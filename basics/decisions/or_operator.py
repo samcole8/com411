780cc3b21c16ea2bd288167022681cf392dd4667
samcole8 <sam@samcole.me>
2022-10-19 18:34:34 +0100

    Added code to or_operator.py

---
+++ basics/decisions/or_operator.py
@@ -0,0 +1,11 @@
+"""
+Demonstrates use of logical or operator
+"""
+
+type_of_adventure = input("")
+if (type_of_adventure == "scary") or (type_of_adventure == "short"):
+    print("Entering the dark forest!")
+elif (type_of_adventure == "long") or (type_of_adventure == "safe"):
+    print("Taking the safe route!")
+else:
+    print("Not sure which route to take.")
