4349220b73e98713b54cf18e2e3e3f6462ebf572
samcole8 <sam@samcole.me>
2022-10-19 13:39:32 +0100

    Created comparison_operators.py and added code to demonstrate the use of comparison operators.

---
+++ basics/decisions/comparison_operators.py
@@ -0,0 +1,10 @@
+"""
+Use comparison operators to determine which number is greater.
+"""
+
+number_one = input("Please enter the first number:\n")
+number_two = input("Please enter the second number:\n")
+if number_one > number_two:
+    print("The first number is greatest.")
+else:
+    print("The second number is greatest.")
