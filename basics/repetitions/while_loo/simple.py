2c93b8237ee2cd38b26780522cd987df265c4371
samcole8 <sam@samcole.me>
2022-10-19 18:52:30 +0100

    Added code to simple.py

---
+++ basics/repetitions/while_loo/simple.py
@@ -0,0 +1,9 @@
+"""
+Demonstrates simple while loop
+"""
+
+number_of_cables = int(input("How many cables should I remove?\n"))
+print()
+while number_of_cables > 0:
+    print("Cable removed.")
+    number_of_cables -= 1
