8e50d6119b980db595cc4315227d96aa62bd561d
samcole8 <sam@samcole.me>
2022-10-19 14:08:13 +0100

    Pylint C0103 disabled locally

--- basics/decisions/counter.py
+++ basics/decisions/counter.py
@@ -6,8 +6,8 @@ number_one = input("Please enter the first whole number.\n")
 number_two = input("Please enter the second whole number.\n")
 number_three = input("Please enter the third whole number.\n")
 
-number_of_odds = 0
-number_of_evens = 0
+number_of_odds = 0 # pylint: disable-msg=C0103
+number_of_evens = 0 # pylint: disable-msg=C0103
 if (number_one % 2) == 0:
     number_of_evens += 1
 else:
@@ -20,3 +20,5 @@ if (number_three % 2) == 0:
     number_of_evens += 1
 else:
     number_of_odds += 1
+
+print(f"There are {number_of_odds} odds and {number_of_evens} evens.")
