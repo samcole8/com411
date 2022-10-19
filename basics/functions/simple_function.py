a19ff0e7373a15e108aa3703eb25c35d9b37d0ee
samcole8 <sam@samcole.me>
2022-10-19 23:38:30 +0100

    Added code to simple_function.py

---
+++ basics/functions/simple_function.py
@@ -0,0 +1,8 @@
+"""Script demonstrating a function encapsulating some code."""
+
+def listen():
+    """Example code"""
+    onomatopoeia = input("Enter an onomatopoeic word:\n")
+    print(f"That was a loud {onomatopoeia}!")
+
+listen()
