ad125911cfc7a69612e523d082bea950daee9f4f
samcole8 <sam@samcole.me>
2022-10-19 21:46:41 +0100

    Added code to nested.py

---
+++ basics/repetitions/nested_loop/nested.py
@@ -0,0 +1,12 @@
+"""
+An example of nested for loops
+"""
+
+ASCII_emoji = ":-) " # pylint: disable-msg=C0103
+number_of_rows = int(input("How many rows should I have?\n"))
+number_of_columns = int(input("How many columns should I have?\n"))
+print("Here I go:\n")
+for row in range(0, number_of_rows):
+    for column in range(0, number_of_columns):
+        print(ASCII_emoji, end="")
+    print()
