632aa7fabac4ca02c2b6a02891f7e94a186e0361
samcole8 <sam@samcole.me>
2022-10-19 14:21:13 +0100

    Added code to demonstrate the use of a nested if statement

---
+++ basics/decisions/nested_decision/nested.py
@@ -0,0 +1,7 @@
+"""
+Demonstrates nested if statements.
+"""
+
+if input("What type of cover does the book have?\n") == "soft":
+    if input("Is the book perfect-bound?\n") == "yes":
+        print("Soft cover, perfect bound books are very popular!")
