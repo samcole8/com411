2c37e57ecf04433cc1e638a0a6334ce6935c4760
samcole8 <sam@samcole.me>
2022-10-19 21:20:45 +0100

    Added code to reverse.py

---
+++ basics/repetitions/for_loop/reverse.py
@@ -0,0 +1,10 @@
+"""
+Simple script to reverse a string
+"""
+
+forward_phrase = input("What phrase do you see?\n")
+backward_phrase = "" # pylint: disable-msg=C0103
+print("\nReversing...")
+for character in range(len(forward_phrase), 0, -1):
+    backward_phrase += forward_phrase[character - 1]
+print(f"\nThe phrase is: {backward_phrase}")
