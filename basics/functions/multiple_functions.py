d85f5694a8bf290a971f6196b5af9b00e99e4fd6
samcole8 <sam@samcole.me>
2022-10-20 17:53:18 +0100

    Added code to demonstrate multiple functions.

---
+++ basics/functions/multiple_functions.py
@@ -0,0 +1,19 @@
+""" Code demonstrating multiple functions.
+"""
+
+def display_ladder(steps):
+    """ Print a string based on the parameter
+    """
+    for _i in range(0, steps):
+        print("| |")
+        print("***")
+
+
+def create_ladder():
+    """ Take input for steps and call function
+    """
+    steps = int(input("How many steps remain?"))
+    display_ladder(steps)
+
+
+create_ladder()
