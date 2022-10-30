5e9d38ae8c0effbd22bff81a397b87e8971d1055
samcole8 <sam@samcole.me>
2022-10-30 17:18:49 +0000

    Added code to write to a json file

--- data/files/json/export_json.py
+++ data/files/json/export_json.py
@@ -1,9 +1,30 @@
-"""Docstring."""
+"""Write to a json file."""
+
+
+import json
+
+
+def read(path):
+    """Read from json file."""
+    print("Reading...")
+    with open(path, encoding="utf-8") as file:
+        data = json.load(file)
+    print("...Done!")
+    return data
+
+
+def save(path, data):
+    """Write to json file."""
+    print("Exporting...")
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4)
 
 
 def run():
     """Run the program."""
-    # Do stuff
+    path = "/home/sam/mygit/com411/data/files/json/exported.json"
+    data = read("/home/sam/mygit/com411/data/files/json/robocity.json")
+    save(path, data)
 
 
 if __name__ == "__main__":
