ec839e8e5f7b61324b3c92e4bc405176ebaee98a
samcole8 <sam@samcole.me>
2022-11-04 13:31:47 +0000

    Added features

--- data/titanic/titanic.py
+++ data/titanic/titanic.py
@@ -14,7 +14,7 @@ def load_data(file_path):
     # Open CSV file
     with open(file_path, encoding="utf-8") as file:
         csv_reader = csv.reader(file)
-        headings.append(csv_reader)  # Read CSV headings into global variable
+        headings.append(next(csv_reader))  # Read CSV headings
         # Read remaining records
         for record in csv_reader:
             records.append(record)
@@ -30,6 +30,7 @@ Please select one of the following options:
   [2] Display the number of passengers that survived
   [3] Display the number of passengers per gender
   [4] Display the number of passengers per age group
+  [5] Display the number of survivors per age group
   \n: """))
     return choice
 
@@ -52,6 +53,66 @@ def display_num_survivors():
     input("\nPress ENTER to return to menu.")
 
 
+def display_passengers_per_gender():
+    """Display number of passengers per gender from records."""
+    males = 0
+    females = 0
+    for record in records:
+        if record[4] == "male":
+            males += 1
+        else:
+            females += 1
+    print(f"Females: {females}, males: {males}")
+    input("\nPress ENTER to return to menu.")
+
+
+def display_passengers_per_age_group():
+    """Display number of passengers per age group from records."""
+    children = 0
+    adults = 0
+    elderly = 0
+    for record in records:
+        if record[5] == "":
+            pass
+        elif float(record[5]) < 18:
+            children += 1
+        elif float(record[5]) > 65:
+            elderly += 1
+        else:
+            adults += 1
+    print(f"Children: {children}, adults: {adults}, elderly: {elderly}")
+    input("\nPress ENTER to return to menu.")
+
+
+def display_survivors_per_age_group():
+    """Display number of survivors per age group from records."""
+    children_s = 0
+    children = 0
+    adults_s = 0
+    adults = 0
+    elderly_s = 0
+    elderly = 0
+    for record in records:
+        if record[5] == "":
+            pass
+        elif float(record[5]) < 18:
+            if record[1] == "1":
+                children_s += 1
+            children += 1
+        elif float(record[5]) > 65:
+            if record[1] == "1":
+                adults_s += 1
+            adults += 1
+        else:
+            if record[1] == "1":
+                elderly_s += 1
+            elderly += 1
+        print(children)
+    print(f"Children: {children_s}/{children}, adults: {adults_s}/{adults}" +
+          f", elderly: {elderly_s}/{elderly}")
+    input("\nPress ENTER to return to menu.")
+
+
 def run():
     """Run the program."""
     load_data("/home/sam/mygit/com411/data/titanic/titanic.csv")
@@ -62,6 +123,12 @@ def run():
             display_passenger_names()
         elif choice == 2:
             display_num_survivors()
+        elif choice == 3:
+            display_passengers_per_gender()
+        elif choice == 4:
+            display_passengers_per_age_group()
+        elif choice == 5:
+            display_survivors_per_age_group()
         else:
             print("Error: No corresponding function.\n")
 
