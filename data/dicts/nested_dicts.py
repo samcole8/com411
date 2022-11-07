9776097a0e90e48cb8674b704a82bbe832ba11fb
samcole8 <sam@samcole.me>
2022-11-07 14:35:27 +0000

    Added code demonstrating the use of nested loops

--- data/dicts/nested_dicts.py
+++ data/dicts/nested_dicts.py
@@ -1,9 +1,36 @@
 """Demonstrates nested dictionaries."""
 
 
+def short_pattern():
+    """Create a short dictionary."""
+    pattern = {"sequence": "101", "occurrences": 5}
+    return pattern
+
+
+def medium_pattern():
+    """Create a medium dictionary."""
+    pattern = {"sequence": "111000", "occurrences": 25}
+    return pattern
+
+
+def long_pattern():
+    """Create a long dictionary."""
+    pattern = {"sequence": "1100110011001100", "occurrences": 50}
+    return pattern
+
+
 def run():
     """Run the program."""
-    # Do stuff
+    print("Analysing patterns...")
+    # Create nested dictionary
+    pattern_dict = {
+                    "short sequence": short_pattern(),
+                    "medium sequence": medium_pattern(),
+                    "long sequence": long_pattern(),
+                    }
+    # Display contents of pattern_dict
+    for key, value in pattern_dict.items():
+        print(f"{key}: {value}")
 
 
 if __name__ == "__main__":
