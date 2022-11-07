a986cadc27c2291f830bdb11335eedb9eea2e4ee
samcole8 <sam@samcole.me>
2022-11-07 11:25:38 +0000

    Added code to create dictionary.

---
+++ data/dicts/simple_dict.py
@@ -0,0 +1,16 @@
+"""Create dictionary."""
+
+
+def pattern():
+    """Create and return dictionary."""
+    sequences = {"Short Sequence": 3, "Medium Sequence": 2, "Long Sequence": 1}
+    return sequences
+
+
+def run():
+    """Run the program."""
+    print(pattern())
+
+
+if __name__ == "__main__":
+    run()
