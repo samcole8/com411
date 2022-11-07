ed2ad81f50b37f3b5f6a546b4c07daddec75f9a7
samcole8 <sam@samcole.me>
2022-11-07 11:14:56 +0000

    Added code demonstrating removing set usage.

--- data/sets/sorted_set.py
+++ data/sets/sorted_set.py
@@ -6,17 +6,44 @@ def observed():
     observations = []
     # Create list from user input
     for _i in range(5):
-        user_input = int(input("Please enter an observation:  "))
+        user_input = input("Please enter an observation:  ")
         observations.append(user_input)
     return observations
 
 
 def remove_observations(observation_list):
-    user_input = input("Do you wish to remove observations? [y/n]: ")
+    """Remove object from a list."""
+    # Loop until user quits
+    loop_on = True
+    while loop_on:
+        # Branch by user input
+        user_input = input("\nDo you wish to remove observations? [Y/n]: ")
+        if user_input in ("y", "", "Y"):
+            # Remove item from list
+            user_input = input("What observation do you wish to remove?")
+            observation_list.remove(user_input)
+            print(f"Removed {user_input} from the list.")
+        else:
+            # Exit loop
+            print()
+            loop_on = False
+
 
 def run():
     """Run the program."""
-    # Do stuff
+    # Create list and allow user to remove items.
+    observation_list = observed()
+    remove_observations(observation_list)
+    # Create set
+    observation_set = set()
+    # Add each item, and the number of times it appears to the set as a tuple.
+    for value in observation_list:
+        value_count = observation_list.count(value)
+        observation_set.add((value, value_count))
+    # Output contents of set
+    print("Observations:")
+    for value in observation_set:
+        print(f"{value[0]} was observed {value[1]} times.")
 
 
 if __name__ == "__main__":
