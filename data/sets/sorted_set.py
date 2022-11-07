70b956e2236058c8efa8ce67e42f9ad095320ffa
samcole8 <sam@samcole.me>
2022-11-07 11:01:49 +0000

    Small fix.

---
+++ data/sets/sorted_set.py
@@ -0,0 +1,23 @@
+"""Docstring."""
+
+
+def observed():
+    """Return set."""
+    observations = []
+    # Create list from user input
+    for _i in range(5):
+        user_input = int(input("Please enter an observation:  "))
+        observations.append(user_input)
+    return observations
+
+
+def remove_observations(observation_list):
+    user_input = input("Do you wish to remove observations? [y/n]: ")
+
+def run():
+    """Run the program."""
+    # Do stuff
+
+
+if __name__ == "__main__":
+    run()
